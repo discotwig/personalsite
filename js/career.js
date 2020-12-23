82bf71e852028f38419f3eb84e7531fac463a23c
Ryan <bishopryant@gmail.com>
2020-12-23 12:16:59 -0500

    working modals

--- js/career.js
+++ js/career.js
@@ -1,25 +1,24 @@
 // JavaScript Document
+//
 
-function openModal(clicked_id) {
-	var modal_id = clicked_id.replace("button","modal");
-	window.alert(modal_id);
-	
-	var modal = document.getElementById(modal_id);
-	window.alert(modal);
-	modal.style.display = "inline-block";
-	
-//	var span = document.getElementsByClassName("close")[0];
-//	// When the user clicks on <span> (x), close the modal
-//	span.onclick = function() {
-//		modal.style.display = "none";
-//	}
-//	// When the user clicks anywhere outside of the modal, close it
-//	window.onclick = function(event) {
-//		if (event.target == modal) {
-//			modal.style.display = "none";
-//		}
-//	}	
+// onClick for spans
+function closeModal(modal_id) {
+	document.getElementById(modal_id).style.display = 'none';
 }
 
+// same thing, one liner
+//let closeModal = modal_id => document.getElementById(modal_id).style.display = 'none';
 
-
+function openModal(clicked_id) {
+	var modal_id = clicked_id.replace('button', 'modal');
+	//	window.alert(modal_id);
+	var modal = document.getElementById(modal_id);
+	//	window.alert(modal);
+	modal.style.display = 'inline-block';
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = 'none';
+		}
+	}
+}
